f099b7deb72b93e31249781a74818243ff1c752e
lbingd17 <lbingd17@mail.ustc.edu.cn>
2016-08-28 14:30:02 +0800

    Update a002StringBasics.java

--- a002StringBasics.java
+++ a002StringBasics.java
@@ -1,9 +1,12 @@
-public static void main(){
-		String x = "a,1,2,3,4";
+
+public class a002StringBasics {
+	public static void main(){
+		String x = "a,1,asdf,2,3,4";
 		String[] b = x.split(",");
 		for(String ttx:b){
 			System.out.println(ttx);
 		}
 
 
+	}
 }
