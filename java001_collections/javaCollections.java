107aca6bf2e3961c182fad7e864916cd4c5da2b8
lbingd17 <lbingd17@mail.ustc.edu.cn>
2016-08-29 16:04:22 +0800

    Create javaCollections.java

---
+++ java001_collections/javaCollections.java
@@ -0,0 +1,7 @@
+/**
+Reference:
+http://www.cnblogs.com/xwdreamer/archive/2012/05/30/2526822.html
+http://blog.csdn.net/zhangerqing/article/details/8122075
+http://blog.csdn.net/zhangerqing/article/details/8193118
+**/
+
